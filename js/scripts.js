703f410f546fd80c33ce0250abde2b1aa471958e
Laurie Waller <lauriewaller7@gmail.com>
2021-02-13 22:09:20 -0800

    remove comments

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,15 @@ $(document).ready(function() {
     event.preventDefault();
 
     const userInput = $("input#faveNumber").val();
-    console.log(userInput)
-    //let number = "23"
     let array = [];
 
     for (let i = 0; i <= userInput; i++) {
       array.push(i);
     } 
-    //console.log(array)
-    let newArray = array.map(function(element){return element.toString()});
-    //console.log(newArray); 
+
+    let newArray = array.map(function(element){
+      return element.toString()
+    });
 
     let beepBoop = newArray.forEach(function(i) {
       if (newArray[i].includes("3")) {
